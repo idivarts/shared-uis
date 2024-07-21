e93f1db2b1e7a9c83b8521a92143de1b81af0505
Rahul Sinha <rahul.sinha1908@gmail.com>
2024-07-21 20:39:27 +0530

    Printing welcome

--- TestUI.tsx
+++ TestUI.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const TestUI = () => {
+    useEffect(() => {
+        console.log("Welcome");
+    }, [])
     return (
         <div>TestUI</div>
     )
