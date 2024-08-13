0936ddd2a0c6f2be8f6ec85d6edef77f4608392e
dev-rathore <rathoredvd3001@gmail.com>
2024-08-13 17:17:40 +0530

    Reusable image picker

---
+++ constants/Colors.ts
@@ -0,0 +1,27 @@
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#fff";
+
+export default {
+  light: {
+    text: "#000",
+    background: "#fff",
+    tint: tintColorLight,
+    tabIconDefault: "#ccc",
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: "#fff",
+    background: "#000",
+    tint: tintColorDark,
+    tabIconDefault: "#ccc",
+    tabIconSelected: tintColorDark,
+  },
+  regular: {
+    primary: "#2f95dc",
+    orange: "#F64740",
+    white: "#fff",
+    lightgray: "lightgray",
+    black: "#000",
+    backdrop: "rgba(0, 0, 0, 0.5)",
+  },
+};
