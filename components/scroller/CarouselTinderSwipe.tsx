3f21c43e1c6cec1e277b6e39cfc6e4929cd6d0f1
Rahul Sinha <rahul@idiv.in>
2025-06-19 00:53:23 +0530

    Implemented Tinder swiper

---
+++ components/scroller/CarouselTinderSwipe.tsx
@@ -0,0 +1,77 @@
+import { Console } from '@/shared-libs/utils/console';
+import React, { useEffect, useState } from 'react';
+import Carousel, { CarouselRenderItem } from 'react-native-reanimated-carousel';
+
+interface IProps<T = any> {
+    data: T[];
+    renderItem: CarouselRenderItem<T>;
+    objectKey: string;
+    width: number;
+    height: number;
+    onLoadMore?: () => void;
+}
+const CarouselTinderSwipe: React.FC<IProps> = (props) => {
+    const [data, setData] = useState<any[]>([])
+    useEffect(() => {
+        if (!props.data || props.data.length < 3) {
+            Console.error("CarouselScroller requires at least 3 items to function properly", "CarouselScroller");
+            return;
+        }
+        setData([props.data[0], props.data[1], props.data[1]]);
+        Console.log("CarouselScroller initialized with data length:", props.data.length);
+    }, [props.data])
+
+    if (props.data.length < 3) {
+        Console.error("CarouselScroller requires at least 3 items to function properly", "CarouselScroller");
+        return null
+    }
+
+    const refreshCarousel = (index: number) => {
+        Console.log("Refreshing carousel at index:", index);
+        // if (!data[index])
+        //     return;
+        const key = data[index][props.objectKey]
+        const globalIndex = props.data.findIndex((item) => item[props.objectKey] === key)
+        if (globalIndex == -1)
+            return;
+        if (globalIndex >= data.length - 3) {
+            props.onLoadMore?.();
+        }
+        setData((prevData) => {
+            const newData = [...prevData];
+            // const itemToMove = newData.splice(globalIndex, 1)[0];
+            // newData.push(itemToMove);
+            let previousIndex = index - 1;
+            if (previousIndex < 0)
+                previousIndex = data.length - 1;
+            let nextIndex = index + 1;
+            if (nextIndex >= data.length)
+                nextIndex = 0;
+
+            if (globalIndex + 1 < props.data.length) {
+                newData[previousIndex] = props.data[globalIndex + 1];
+                newData[nextIndex] = props.data[globalIndex + 1];
+            }
+            return [...newData];
+        });
+    }
+
+    return (<Carousel
+        loop={true}
+        vertical={false}
+        onSnapToItem={refreshCarousel}
+        width={props.width}
+        height={props.height}
+        data={data}
+        renderItem={props.renderItem}
+        mode="parallax"
+        modeConfig={{
+            parallaxScrollingScale: 0.9,
+            parallaxScrollingOffset: 50,
+            parallaxAdjacentItemScale: 0.4,
+        }}
+    />)
+
+}
+
+export default CarouselTinderSwipe
